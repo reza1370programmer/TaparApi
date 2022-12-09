21aa67debb6e0d52769dbad66140344db469aa9c
reza1370programmer <78476427+reza1370programmer@users.noreply.github.com>
2022-12-09 12:40:22 +0330

    nn

--- Tapar.Core/Common/Dtos/TagDto.cs
+++ Tapar.Core/Common/Dtos/TagDto.cs
@@ -13,4 +13,12 @@ namespace Tapar.Core.Common.Dtos
         public string title { get; set; }
         public long id { get; set; }
     }
+    public class AddTagDto
+    {
+        [Required(ErrorMessage = "عنوان تگ را وارد کنید")]
+        [StringLength(20, ErrorMessage = "تعداد کاراکترها نمیتواند بیشتر از 20 تا باشد")]
+        public string title { get; set; }
+        [Required(ErrorMessage = "دسته بندی سطح سوم را انتخاب کیند")]
+        public int cat3Id { get; set; }
+    }
 }
--- TaparApi/Controllers/TagController.cs
+++ TaparApi/Controllers/TagController.cs
@@ -12,29 +12,58 @@ namespace TaparApi.Controllers
 
     public class TagController : BaseController
     {
-        public ITagRepository repository { get; set; }
+        public IRepository<Tag> repository { get; set; }
         public IRepository<TagCat3> tagCat3Repository { get; set; }
 
-        public TagController(ITagRepository repository, IRepository<TagCat3> tagCat3Repository)
+        public TagController(IRepository<Tag> repository, IRepository<TagCat3> tagCat3Repository)
         {
             this.repository = repository;
             this.tagCat3Repository = tagCat3Repository;
         }
-        [HttpGet("[action]")]
-        public async Task<IActionResult> GetAllTags()
+        [HttpGet("[action]/{searchkey?}")]
+        public async Task<IActionResult> SearchTags(string? searchkey,CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var tags = await repository.TableNoTracking.Where(t => t.title.Contains(searchkey)).Select(t => new {t.title,t.Id}).ToListAsync(cancellationToken);
+            return Ok(tags);
         }
-        [HttpGet("[action]/{title}")]
-        public async Task<IActionResult> AddTag(string title, CancellationToken cancellationToken)
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AddTag(AddTagDto dto, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var role = User.FindFirstValue(ClaimTypes.Role);
+            if (role == "superadmin" && UserIsAutheticated)
+            {
+                if(ModelState.IsValid)
+                {
+                    var checkTag = await repository.TableNoTracking.AnyAsync(p => p.title.Trim() == dto.title.Trim());
+                    if (checkTag)
+                    {
+                        var tag = await repository.TableNoTracking.SingleOrDefaultAsync(t => t.title.Trim() == dto.title.Trim());
+                        if (await tagCat3Repository.TableNoTracking.AnyAsync(t=>t.tagId==tag.Id && dto.cat3Id==t.cat3Id)) {
+                            return Ok(null);
+                        }
+                        await tagCat3Repository.AddAsync(new TagCat3 { tagId = tag.Id, cat3Id = dto.cat3Id },cancellationToken);
+                        return RedirectToAction("GetTagsByCat3Id", new { dto.cat3Id, cancellationToken });
+                    }
+                    else
+                    {
+                        var tag = new Tag() { title=dto.title};
+                        await repository.AddAsync(tag, cancellationToken);
+                        var tagcat3=new TagCat3() { cat3Id=dto.cat3Id,tagId=tag.Id};
+                        await tagCat3Repository.AddAsync(tagcat3,cancellationToken);
+                        return RedirectToAction("GetTagsByCat3Id", new { dto.cat3Id, cancellationToken });
+                    }
+                }
+                return BadRequest();
+            }
+            return Unauthorized();
+               
+            
         }
         [HttpGet("[action]/{cat3Id}")]
         public async Task<IActionResult> GetTagsByCat3Id(int cat3Id, CancellationToken cancellationToken)
         {
             var cat3Tags = await tagCat3Repository.TableNoTracking.Where(p => p.cat3Id == cat3Id)
-                .Select(p =>new { p.tag.Id,p.tag.title })
+                .Select(p => new { p.tag.Id, p.tag.title })
                 .ToListAsync(cancellationToken);
             return Ok(cat3Tags);
         }
@@ -44,7 +73,7 @@ namespace TaparApi.Controllers
             var role = User.FindFirstValue(ClaimTypes.Role);
             if (role == "superadmin" && UserIsAutheticated)
             {
-                if(ModelState.IsValid)
+                if (ModelState.IsValid)
                 {
                     var tag = await repository.GetByIdAsync(cancellationToken, dto.id);
                     tag.title = dto.title;
@@ -52,10 +81,10 @@ namespace TaparApi.Controllers
                     return RedirectToAction("GetTagsByCat3Id", new { dto.cat3Id, cancellationToken });
                 }
                 return BadRequest();
-     
+
             }
             return Unauthorized();
-       
+
         }
     }
 }
